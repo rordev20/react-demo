beeb125870883258ee7ffbe68a335833d4c11b14
Rohit Patel <rohit.patel@infibeam.net>
2019-10-11 16:48:20 +0530

    added cart component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, {Fragment} from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Cart from './components/cart/Cart';
+
 import './App.css';
 
 const App = () => (
-  <Fragment>
-    <Navbar />
-    <Landing />
-  </Fragment>
+	<Router>
+	  <Fragment>
+	    <Navbar />
+	    <Route exact path='/' component={Landing} />
+	    <section className='container'>
+	      <Switch>
+          <Route exact path='/cart' component={Cart} />
+	      </Switch>
+	    </section>
+	  </Fragment>
+  </Router>
 );
 
 
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,19 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className="navbar bg-dark">
       <h1>
-        <a href="index.html">
+        <Link to="/">
           <i className="fas fa-code">
           </i> Project Logo Here
-        </a>
+        </Link>
       </h1>
       <form className="example serach-form" action="">
         <input type="text" name="search2" className="form-control"></input>
         <button type="submit"><i className="fa fa-search"></i></button>
       </form>
-
+      <ul>
+        <li>
+          <Link to="/cart">Cart</Link>
+        </li>
+      </ul>
     </nav>
   )
 }
